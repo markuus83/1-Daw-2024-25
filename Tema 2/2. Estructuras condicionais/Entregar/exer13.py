432d4cd5230c4b7665424196a3afb38821b76c79
daw1 - Chouza Cruces <A23MarcosCC@sanclemente.local>
2024-09-30 12:20:06 +0200

    Exercicios EstructurasCondicionais terminados

--- Tema 2/2. Estructuras condicionais/Entregar/exer13.py
+++ Tema 2/2. Estructuras condicionais/Entregar/exer13.py
@@ -8,3 +8,46 @@ A continuación preguntaralle ao usuario se a tarifa é en bruto ou en neto. A p
 """
 
 __author__ = "Marcos Chouza Cruces"
+
+#Importamos unha librería. Non é precisa para o correcto funcionamento do código
+import time
+
+#Supoñemos, que o usuario sempre traballa a mesma cantidade de horas ao día
+
+#Pedimoslle ao usuario que introduza os datos precisos
+horas_traballadas = int(input("Ingrese a cantidade de horas traballadas: "))
+irpf = float(input("Ingrese a porcentaxe de desconto IRPF: "))
+
+#usamos a importación da librería, aínda que non é precisa
+time.sleep(1)
+
+#Comezamos coa estrutura condicional, para calcular o seu salario mensual dependendo das diferentes circunstancias
+print("\nAgora, imos seguir cos cálculos precisos...")
+print("\ta) A miña tarifa é neta: ")
+print("\tb) A miña tarifa é bruta: ")
+
+#Establecemos a opción que queremos utilizar
+opcion = input(">")
+
+#O primeiro 'IF' serve para dar o salario mensual cando xa está o desconto do IRPF no salario por horas
+if opcion == 'a':
+    
+    tarifa_neta = float(input("Ingrese a cantidade de cartos a recibir por hora: "))
+    
+    salario_diario = horas_traballadas*tarifa_neta
+    salario_total = 22*salario_diario
+    print("O salario mensual do usuario é de:", salario_total)
+
+
+#O segundo 'IF' serve para dar o salario mensual cando NON está descontado o IRPF no salario por horas
+elif opcion == 'b':
+    
+    tarifa_bruta = float(input("Ingrese a cantidade de cartos a recibir por hora: "))
+    salario_diario = horas_traballadas*tarifa_bruta
+    salario_descontado = salario_diario - salario_diario*(irpf/100)
+    salario_total = salario_descontado = 22*salario_descontado
+    print("O salario mensual do usuario é:", salario_total)
+
+#O último 'IF' encárgase de amosar posibles erros do código
+else:
+    print("Erro")

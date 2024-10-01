0479f15fccd13939cc6f5b595e20acb89278fb05
Marcos <marcoschouza@gmail.com>
2024-10-01 20:56:31 +0200

    Ejercicios 8 y 9 hechos, faltan comentarios

---
+++ Programacion/Tema 2/3. Estruturas iterativas/Entregar/exer11.py
@@ -0,0 +1,7 @@
+""" 
+Escribe un script que calcule o mínimo común múltiplo de dous números introducidos polo usuario:
+
+    · Non utilices ningún algoritmo, vai probando números ata que atopes un número que sexa múltiplo dos dous números.
+"""
+
+__author__ ="Marcos Chouza Cruces"

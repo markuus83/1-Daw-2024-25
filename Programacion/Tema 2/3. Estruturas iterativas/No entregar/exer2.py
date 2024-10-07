edcbe30d40f7995b5a27407c64fb6ad038ed0f8b
Marcos Chouza Cruces <a23MarcosCC@iessanclemente.net>
2024-10-07 12:38:14 +0200

    Prueba Terminator

--- Programacion/Tema 2/3. Estruturas iterativas/No entregar/exer2.py
+++ Programacion/Tema 2/3. Estruturas iterativas/No entregar/exer2.py
@@ -4,11 +4,3 @@ Escribe un script que imprima os números pares (un en cada liña) ata un númer
 
 __author__ ="Marcos Chouza Cruces"
 
-numero = int(input("Ingrese un numero enteiro positivo: "))
-
-contador = 0
-
-while contador <= numero:
-    print(contador)
-    contador += 2
-    

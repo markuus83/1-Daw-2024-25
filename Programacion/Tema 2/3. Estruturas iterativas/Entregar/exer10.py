da39f2d8e5ad36caadb779a5886d5ebb275b5e7c
Marcos <a23marcoscc@iessanclemente.net>
2024-10-13 23:36:32 +0200

    Llevar a clase

--- Programacion/Tema 2/3. Estruturas iterativas/Entregar/exer10.py
+++ Programacion/Tema 2/3. Estruturas iterativas/Entregar/exer10.py
@@ -1,5 +1,18 @@
 """ 
-Escribe un script no que o usuario poida introducir números enteiros por teclado ata que tecleea palabra "fin". Tras finalizar a introdución de números, indícalle cal é o número máis pequeno introducido.
+Escribe un script no que o usuario poida introducir números enteiros por teclado ata que teclee a palabra fin"fin". Tras finalizar a introdución de números, indícalle cal é o número máis pequeno introducido.
 """
+
 __author__ = "Marcos Chouza Cruces"
 
+while True:
+    
+    linea = input("> ")
+    
+    if linea[0] == '#' :
+        continue
+    
+    if linea == 'fin':
+        break
+    print(linea)
+    
+print('¡Terminado!')

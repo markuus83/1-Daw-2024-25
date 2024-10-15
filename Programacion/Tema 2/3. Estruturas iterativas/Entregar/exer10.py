6565e872d88166e1f469bde17e6232e1acf6f480
Marcos <a23marcoscc@iessanclemente.net>
2024-10-15 12:56:43 +0200

    Exercicios Programación

--- Programacion/Tema 2/3. Estruturas iterativas/Entregar/exer10.py
+++ Programacion/Tema 2/3. Estruturas iterativas/Entregar/exer10.py
@@ -11,24 +11,3 @@ contador = 0
 print("No seguinte programa, introduce números para calcular cal vai ser o menor de todos. \nPara saír do programa, escribe 'fin' ")
 
 #Comezamos co bucle iterativo 
-while True:
-    
-    #Pedímoslle ao usuario que ingrese valores
-    numero = input("\nIngrese un valor: ")
-    
-    #Rematamos o pedirlle novos valores cando ingrese 'fin'
-    if numero == 'fin':
-        if contador == 0:
-            print("Erro, non se ingresaron valores")
-        break
-    
-    #Casteamos la variable a un tipo INT
-    numero = int(numero)
-    contador += 1
-    
-    #Cambiamos o valor da primeira variable para que vaia adoptando o valor máis pequeno introducido
-    if numero < num_menor:
-        num_menor = numero
-
-    print(f"El número introducido más pequeño es: {num_menor}")
-    

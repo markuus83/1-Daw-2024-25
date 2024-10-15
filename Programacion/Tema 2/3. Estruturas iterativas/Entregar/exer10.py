57c8fa68f65165f1d781c63eaf1f92ed12a2698e
Marcos <marcoschouza@gmail.com>
2024-10-15 13:34:47 +0200

    Acabados os exercicios de programacion

--- Programacion/Tema 2/3. Estruturas iterativas/Entregar/exer10.py
+++ Programacion/Tema 2/3. Estruturas iterativas/Entregar/exer10.py
@@ -4,10 +4,37 @@ Escribe un script no que o usuario poida introducir números enteiros por teclad
 
 __author__ = "Marcos Chouza Cruces"
 
-#Inicializamos unha varaible con gran valor
-num_menor = 99999999999999999
+#Inicializamos as variables necesarias para o código
 contador = 0
+#Inicializamos unha variable con valor infinito para que os numeros ingresados, por moi grandes que sexan, sempre sean menores
+num_menor = float('inf')
 
-print("No seguinte programa, introduce números para calcular cal vai ser o menor de todos. \nPara saír do programa, escribe 'fin' ")
+#Comezamos co primeiro bucle iterativo
+while True:
+    
+    #Inicializamos o segundo bucle
+    while True:
+        numero = input("Introduce un valor: ")
+        
+        #O programa apra cando ingresamos o valor fin
+        if numero == 'fin':
+            break
+        #Casteamos a int os valores de string ingresados
+        numero = int(numero)
 
-#Comezamos co bucle iterativo 
+        #Almacenamos os valores máis pequenos nunha variable
+        if numero < num_menor:
+            num_menor = numero
+        
+        #Aumentamos o valor da variable iterativa, para saber se se ingresaron valores ou non
+        contador += 1
+    
+    if numero == 'fin':
+        break
+
+
+#Damos a saída por pantalla
+if contador == 0:
+    print("Erro, non se introduciron valores.")
+else:
+    print(f"O número máis pequeno introducido é: {num_menor}")
--- Programacion/Tema 2/3. Estruturas iterativas/Entregar/exer12.py
+++ Programacion/Tema 2/3. Estruturas iterativas/Entregar/exer12.py
@@ -1,5 +1,5 @@
 """ 
-Escribe un script que elexirá no seu comezo un número ao azar entre 1 e 25. A continuación o usuario introducirá números por teclado ata que acerte o nñumero seleccionado ao azar. Cada vez que se introduza un número incorrecto, o script proporcionaralle pistas ao usuario: "o número é maior" ou "o número é menor". Unha vez que o usuario acerte o númeero, indicaráselle por pantalla se gañou o xogo ou non. Para ganar, o usuario deberá acertar o número en menos de 5 intentos.
+Escribe un script que elexirá no seu comezo un número ao azar entre 1 e 25. A continuación o usuario introducirá números por teclado ata que acerte o nñumero seleccionado ao azar. Cada vez que se introduza un número incorrecto, o script proporcionaralle pistas ao usuario: "o número é maior" ou "o número é menor". Unha vez que o usuario acerte o número, indicaráselle por pantalla se gañou o xogo ou non. Para ganar, o usuario deberá acertar o número en menos de 5 intentos.
 """
 
 __author__ = "Marcos Chouza Cruces"

01f1ca2c4f4a3136514f409713000268b72a647e
Marcos <marcoschouza@gmail.com>
2024-10-28 21:17:46 +0100

    Exercicios Programacion Cadeas

--- Programacion/Tema 2/6. Cadeas/No entregar/exer3.py
+++ Programacion/Tema 2/6. Cadeas/No entregar/exer3.py
@@ -6,3 +6,35 @@ Escribe un script que elimine todas as consoantes dunha cadea de texto introduci
 """
 
 __author__ = "Marcos Chouza Cruces"
+
+#Inicializamos unha variable con valores que queremos comparar
+consoantes = "bcdfghjklmnñpqrstvwxyzçBCDFGHJKLMNÑPQRSTVWXYZÇ"
+
+def eliminar_consoantes(cadea: str) -> str:
+    """
+    ELiminamos as consoantes presentes na cadea a traballar
+
+    Args:
+        cadea (str): Cadea de caracteres proporcionada polo usuario
+
+    Returns:
+        str: Nova cadea sen consoantes
+    """
+    
+    nova_cadea = ""
+    
+    for caracter in cadea:
+        
+        if caracter not in consoantes:
+            nova_cadea += caracter
+            
+    return nova_cadea
+        
+#Pedímoslle ao usuario que ingrese os datos precisos
+cadea = input("Ingrese unha cadea de texto: ")
+
+#Almacenamos o valor da función nunha nova variable para facilitar a lexibilidade do código
+nova_cadea = eliminar_consoantes(cadea)
+
+#Imprimimos por pantalla
+print(f"A cadea final sen consoantes é: {nova_cadea}")
--- Programacion/Tema 2/6. Cadeas/No entregar/exer4.py
+++ Programacion/Tema 2/6. Cadeas/No entregar/exer4.py
@@ -4,5 +4,24 @@
 """ 
 Escribe un script en pyhton que conte o número de palabras nunha cadea de texto introducida por teclado e o mostre por pantalla. Considera que as palabras están separadas por un ou máis espazos. Coidado cos dobres, triples ou máis espazos ao comezo e ao final do texto.
 """
+#################################################
+# Pedir cadena
+# Quitarle los espacios del principio y del final
+# Quitarle los espacios de por medio
+# Medir longitud de la cadena final
+#################################################
 
 __author__ = "Marcos Chouza Cruces"
+
+def eliminar_espazos_incial_final(cadea: str) -> str:
+    c = cadea.strip()
+    return c
+
+def contar_palabras(c: str) -> str:
+    
+    return
+cadea = input("Ingrese unha cadea de texto: ")
+
+c = eliminar_espazos_incial_final(cadea)
+
+print(f"A cadea final")

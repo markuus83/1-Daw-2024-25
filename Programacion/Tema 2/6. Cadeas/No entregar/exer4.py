7c91360b2b2aae561d1da8aec82cf5d6ca8da4a1
Marcos <a23marcoscc@iessanclemente.net>
2024-10-29 12:28:06 +0100

    Programacion

--- Programacion/Tema 2/6. Cadeas/No entregar/exer4.py
+++ Programacion/Tema 2/6. Cadeas/No entregar/exer4.py
@@ -4,12 +4,12 @@
 """ 
 Escribe un script en pyhton que conte o número de palabras nunha cadea de texto introducida por teclado e o mostre por pantalla. Considera que as palabras están separadas por un ou máis espazos. Coidado cos dobres, triples ou máis espazos ao comezo e ao final do texto.
 """
-#################################################
-# Pedir cadena
+##################################################
+# Pedir cadena     
 # Quitarle los espacios del principio y del final
 # Quitarle los espacios de por medio
 # Medir longitud de la cadena final
-#################################################
+##################################################
 
 __author__ = "Marcos Chouza Cruces"
 

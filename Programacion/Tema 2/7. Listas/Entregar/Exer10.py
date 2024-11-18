f3042131131d290b80dfff40eb3cbbf29b2c93ea
Marcos <marcoschouza@gmail.com>
2024-11-18 20:09:01 +0100

    Programacion

--- Programacion/Tema 2/7. Listas/Entregar/Exer10.py
+++ Programacion/Tema 2/7. Listas/Entregar/Exer10.py
@@ -16,10 +16,49 @@ Escribe unha función en python <burbulla(lista: List) -> List> que implante o a
 
 __author__ = "Marcos Chouza Cruces"
 
-def burbulla(lista: list) -> list:
-    return lista
-    
-lista = [57, 31, 22, 43, 41, 5, 3, 10, 63, 1]
 
-n = burbulla(lista)
-print(n)
+def burbulla(lista: list) -> list:
+    """
+    Ordenamos a lista mediante o método Burbulla
+
+    Args:
+        lista (list): Lista a ordear
+
+    Returns:
+        list: Lista ordeada 
+    """
+    
+    #Calculamos a lonxitude da lista
+    lonxitude = len(lista)
+    
+    #Recorremos a lista indefinidas veces sempre e cando sexa necesario
+    for burbulla in range(lonxitude - 1):
+        
+        # Igualamos unha variable a 'True' para ver se a lista xa se atopa ordenada previamente
+        ordenado = True
+        
+        for indice in range(lonxitude - 1 - burbulla):
+            
+            # Establecemos a condición necesaria 
+            if lista[indice] > lista[indice + 1]:
+                ordenado = False
+
+                #Sobreescribimos os valores para intercambiar os índices
+                variable_auxiliar = lista[indice]
+                lista[indice] = lista[indice + 1]
+                lista[indice + 1] = variable_auxiliar
+
+        #Rompemos o bubcle cando a lista xa se atope ordenada
+        if ordenado:
+            break
+    
+    return lista
+
+# Ingresamos unha lista fixa con valores desordenados
+lista = [1, 3, 5, 10, 22, 31, 41, 43, 57, 63]
+
+# Almacenamos o valor da función nunha variable para mellorar a lexibilidade do código
+lista_ordenada = burbulla(lista)
+
+# Imprimimos a lista ordenada por consola.
+print(f"A lista ordenada polo método BubbleSort é: {lista_ordenada}")
--- Programacion/Tema 2/7. Listas/Entregar/Exer8.py
+++ Programacion/Tema 2/7. Listas/Entregar/Exer8.py
@@ -1,2 +1,126 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+""" 
+Crea un script que pida as notas dun exame dos alumnos dunha clase para procesalos. Tódalas notas téñense que ir almacenando nunha lista. Mostra un menú que se amose continuamente ata que o usuario seleccione a opción saír. As entradas do menú son:
+
+    a) Engadir nota
+    b) ver media
+    c) Ver número de aprobados
+    d) Ver máxima nota
+    e) Eliminar nota -> Primeiro debes de mostrar o menú onde mostres por cada índice da lista a súa nota. O usuario indicará o índice da nota a eliminar.
+    f) Saír
+    
+Para poder realizar cada acción do menú, implanta as seguintes funcións:
+
+    · <engadir_nota(lista: list[float], nota: float)>
+    
+        - Comproba que a lista sexa unha lista, senón lanza a excepción ValueError.
+        - Se a nota é un valor invñalido, lanza a expeción ValueError.
+        
+    · <mostrar_notas(lista: list[float])>
+    
+        - Comproba que a lista sexa unha lista, senón lanza a excepción ValueError.
+        - Débese mostrar neste formato: indice: nota
+        
+            #################
+            #   0 : nota1   #
+            #   1 : nota2   #
+            #################
+            
+    · <media_notas(lista: list[float]) -> float>
+
+        - Comproba que a lista sexa unha lista, senon lanza a excepción ValueError.
+        
+    · <numero_aprobados(lista list[float]) -> int>
+    
+        - Comproba que a lista sexa unha lista, senón lanza excepción ValueError.
+        
+    · <maxima_nota(lista: list[float]) -> float>
+    
+        -Comproba que a lista sexa unha lista, senón lanza excepción ValueError.
+
+    · <eliminar_nota(lista: list[float], indice: int)>
+    
+        - Comproba que a lista sexa unha lista, senñon lanza a excepción ValueError.
+        
+        - Se o índice non é válido lanza a excepción ValueError
+    
+IMPORTANTE: Non utilices para realizar estas duncións do sistema max, min, etc. realiza este cálculo iterando sobre a lista.
+"""
+
+__author__ = "Marcos Chouza Cruces"
+
+def engadir_nota(lista: list[float], nota: float):
+    return lista.append(nota)
+
+
+def mostrar_notas(lista: list[float]):
+    return
+
+
+def media_notas(lista: list[float]) -> float:
+    
+    if type(lista) is not list:
+        raise ValueError("O parámetro non é unha lista. (MEDIA)")
+
+    suma = 0
+
+    for nota in lista: #Calculamos a suma total de todos os elementos da lista
+        suma += nota
+    
+        media = suma /len(lista)
+        
+    return media 
+
+
+def numero_aprobados(lista: list[float]) -> int:
+    return
+
+
+def maxima_nota(lista: list[float]) -> float:
+    return
+
+
+def eliminar_nota(lista: list[float],indice: int):
+    return
+
+
+lista = []
+
+while True: 
+    print("\nPrema a opción desexada no menú.")
+    print("\t a) Engadir nota: ")
+    print("\t b) Ver media: ")
+    print("\t c) Ver número de aprobados: ")
+    print("\t d) Ver máxima nota: ")
+    print("\t e) Eliminar nota: ")
+    print("\t f) Saír")
+    
+    option = input(">")
+    
+    if option == 'a':
+        nota = int(input("Ingrese unha nota: "))
+        
+        
+    elif option == 'b':
+        
+        media = media_notas(lista)
+        
+    elif option == 'c':
+        
+        aprobados = numero_aprobados(lista)
+        
+    elif option == 'd':
+        
+        nota_maxima = maxima_nota(lista)
+        
+    elif option == 'e':
+        
+        indice = input("Ingrese o índice da nota a eliminar: ")
+        eliminar_nota(lista,indice)
+        
+    else:
+        print("Saíndo...")
+        break
+        

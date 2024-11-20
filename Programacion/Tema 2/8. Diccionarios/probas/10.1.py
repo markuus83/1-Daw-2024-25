ee77e73df8495d1cb55977dd60142fcf196f4fe4
Marcos <marcoschouza@gmail.com>
2024-11-20 19:17:34 +0100

    Programacion

---
+++ Programacion/Tema 2/8. Diccionarios/probas/10.1.py
@@ -0,0 +1,15 @@
+en2gal = {'one': 'un', 'two' : 'dous', 'three' : 'tres'}
+
+en2gal['four'] = 'catro'
+
+"""
+print(en2gal['three']) IMPRIME O VALOR DA CHAVE = TRES
+
+print(en2gal['five']) #COMO NON EXISTE ESA CHAVE NO DICCIONARIO, IMPRIME KeyError
+
+__________________________________________________________
+
+valor = en2gal.get('four', 'K tonto no se encuentra en el diccionario')
+
+print(valor)
+"""

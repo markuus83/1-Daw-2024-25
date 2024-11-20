75c7d2eb737df54be0477e02c066523e6b5b0837
Marcos <marcoschouza@gmail.com>
2024-11-20 13:27:29 +0100

    Programacion

--- Programacion/Tema 2/7. Listas/Entregar/Exer8.py
+++ Programacion/Tema 2/7. Listas/Entregar/Exer8.py
@@ -51,8 +51,24 @@ IMPORTANTE: Non utilices para realizar estas duncións do sistema max, min, etc.
 
 __author__ = "Marcos Chouza Cruces"
 
-def engadir_nota(lista: list[float], nota: float):
+def engadir_nota(lista: list[float], nota: float) -> list:
+    """
+    Engadir as notas do alumnado a unha lista
+
+    Args:
+        lista (list[float]): lista onde se almacenan as distintas notas do alumnado
+        nota (float): nota de cada alumno
+
+    Raises:
+        ValueError: O parámetro lista non é unha lista
+        ValueError: O parámetro nota non é un número
+        ValueError: O valor das notas é menor a 0 ou maior a 10
+
+    Returns:
+        list: lista cas notas do alumnado engadidas
+    """
     
+    #Capturamos posibles erros
     if type(lista) is not list:
         raise ValueError("O parámetro non é unha lista. (ENGADIR_NOTA)")
     
@@ -61,20 +77,43 @@ def engadir_nota(lista: list[float], nota: float):
     
     if nota < 0 or nota > 10:
         raise ValueError("A nota non entra nos parámetros. (ENGADIR_NOTA)")
+    
+    #Engadimos o valor da nota na lista
     lista = lista.append(nota)
     
     return lista
 
 
-def mostrar_notas(lista: list[float]):
+def mostrar_notas(lista: list[float]) -> list:
+    
+    """
+    Mostrar as notas do alumnado
+
+    Args:
+        lista (list[float]): lista onde se almacenan as distintas notas do alumnado
+        
+    Raises:
+        ValueError: O parámetro lista non é unha lista
+        ValueError: O valor das notas da lista non é numérico
+
+    Returns:
+        list: índice e notas do alumnado de forma máis visual
+    """
+    #Capturamos posibles erros
     if type(lista) is not list:
         raise ValueError("O parámetro non é unha lista. (MOSTRAR_NOTAS)")
     
+    #Inicializamos unha lista vacía
     notas_formateadas = []
+    
+    #Comezamos cun bucle onde iremos indexando cada elemento co seu valor 
     for indice, nota in enumerate(lista):
+        
+        #Capturamos posibles erros
         if type(nota) is not float:
             raise ValueError("O elemento non é numérico.(MOSTRAR_NOTAS)")
         
+        #Imos engadindo os valores na nova lista
         notas_formateadas.append(f"Índice: {indice}, Valor: {nota}")
     
     return notas_formateadas  
@@ -83,14 +122,34 @@ def mostrar_notas(lista: list[float]):
 
 def media_notas(lista: list[float]) -> float:
     
+    """
+    Calculamos a media das notas da lista
+
+    Raises:
+        ValueError: O parámetro lista non é unha lista
+        ValueError: A listá está vacía, polo que non se pode calcular a súa media
+        ValueError: As notas da lista non son numéricas
+
+    Returns:
+        float: _description_
+    """
+    #Capturamos posibles excepcións
     if type(lista) is not list:
         raise ValueError("O parámetro non é unha lista. (MEDIA)")
 
+    if len(lista) == 0:
+        raise ValueError("Non se pode calcular a media da lista porque está vacía")
+    
+    #inicializamos unha variable axuda
     suma = 0
-
+    
+    #Recorremos cada elemento da lista
     for nota in lista: 
+        
+        #Capturamos  unha posible excepción
         if type(nota) is not float:
             raise ValueError("O elemento non é numérico.(MEDIA)")
+        
         suma += nota
         media = suma /len(lista)
         
@@ -99,14 +158,30 @@ def media_notas(lista: list[float]) -> float:
 
 def numero_aprobados(lista: list[float]) -> int:
     
+    """
+    Devolvemos o número de aprobados da lista
+
+    Raises:
+        ValueError: O parámetro lista non é unha lista
+        ValueError: Os valores da lista non son numéricos
+
+    Returns:
+        int: Número de aprobados
+    """
+    #Capturamos unha posible excepción
     if type(lista) is not list:
         raise ValueError("O parámetro non é unha lista. (APROBADOS)")
     
     aprobado = 0
+    
+    #Recorremos cada elemento da lista
     for nota in lista:
+        
+        #Capturamos unha posible expcepción
         if type(nota) is not float:
             raise ValueError("O elemento non é numérico.(APROBADOS)")
         
+        #Establecemos a condición do aprobado
         if nota >= 5:
             aprobado += 1
             
@@ -115,34 +190,66 @@ def numero_aprobados(lista: list[float]) -> int:
 
 def maxima_nota(lista: list[float]) -> float:
     
+    """
+    Calculamos a nota máxima da lista
+
+    Raises:
+        ValueError: O parámetro lista non é unha lista
+        ValueError: Os valores da lista non son numéricos
+
+    Returns:
+        float: Devolve a nota máxima da lista
+    """
+     #Capturamos unha posible excepción
     if type(lista) is not list:
         raise ValueError("O parámetro non é unha lista. (NOTA_MAXIMA)")
     
     maxima_nota = 0 
+    
+    #Recorremos cada elemento da lista
     for nota in lista:
+        
+        #Capturamos unha posible excepción
+        if type(nota) is not float:
+            raise ValueError("O elemento non é numérico. (NOTA_MÁXIMA)")
+        
+        #Almacenamos nunha variable a nota máis grande 
         if nota > maxima_nota:
             maxima_nota = nota
 
     return maxima_nota
 
 
-def eliminar_nota(lista: list[float],indice: int):
+def eliminar_nota(lista: list[float],indice: int) -> list:
     
+    """
+    Eliminamos unha nota da lista
+
+    Raises:
+        ValueError: O parámetro lista non é unha lista
+        ValueError: O índice non é un valor numérico
+
+    Returns:
+        list: Devolve a lista sen os elementos borrados
+    """
+    #Capturamos posibles excepcións
     if type(lista) is not list:
         raise ValueError("O parámetro non é unha lista. (ELIMINAR_NOTA)")
     
     if type(indice) is not int:
-        raise ValueError("O índice non é un valor numérico")
+        raise ValueError("O índice non é un valor numérico (ELIMINAR_NOTA)")
     
+    #Eliminamos o elemento dependendo do seu índice
     del lista[indice]
     
     return lista
 
-
+#Inicializamos unha lista vacía
 lista = []
 
-while True: 
-    print("\nPrema a opción desexada no menú.")
+#Comezamos co bucle infinito que se amosará por pantalla ata saír del
+while True:
+    print("\n______________________\nPrema a opción desexada no menú.")
     print("\t a) Engadir nota: ")
     print("\t b) Ver media: ")
     print("\t c) Ver número de aprobados: ")
@@ -150,8 +257,10 @@ while True:
     print("\t e) Eliminar nota: ")
     print("\t f) Saír")
     
+    #Establecemos a opción que queremos utilizar
     option = input(">")
     
+    #Lanzamos todas as posibles expecións, imprimimos por consola e chamamos aos métodos dependendo da opción marcada
     try:
         if option == 'a':
         
@@ -165,6 +274,9 @@ while True:
         
         elif option == 'c':
         
+            if len(lista) == 0:
+                raise ValueError("Non se poden ver os aprobados porque aínda non temos notas almacenadas.")
+            
             aprobados = numero_aprobados(lista)
             print(f"O número de aprobados é: {aprobados}")
         
@@ -175,6 +287,9 @@ while True:
             print(f"A nota máxima é: {nota_maxima}")
         
         elif option == 'e':
+            
+            if len(lista) == 0:
+                raise ValueError("Non se pode eliminar elementos porque a lista está vacía.")
             
             notas = mostrar_notas(lista)
             

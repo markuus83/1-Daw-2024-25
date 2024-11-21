ae4a1a4b75da440160e21dee109919d7024c3400
Marcos Chouza Cruces <a23marcoscc@iessanclemente.net>
2024-11-21 12:54:35 +0100

    Programacion

--- Programacion/Tema 2/8. Diccionarios/probas/10.1.py
+++ Programacion/Tema 2/8. Diccionarios/probas/10.1.py
@@ -12,4 +12,32 @@ __________________________________________________________
 valor = en2gal.get('four', 'K tonto no se encuentra en el diccionario')
 
 print(valor)
+
+__________________________________________________________
+
+### VER SI UN VALOR SE ENCUENTRA EN EL DICCIONARIO
+
+Almacenamos el valor en una variable con salida booleana.
+
+x = 'one' in en2gal
+print(x)
+
+__________________________________________________________
+
+### IMPRIMIR LOS VALORES DE UN DICCIONARIO
+
+Convertimos los valores del diccionario en elementos de una lista
+
+value = list(en2gal.values())
+print(value)
+
+__________________________________________________________
+
+### IMPRIMIR LAS CLAVES DE UN DICCIONARIO
+
+Convertimos las claves del diccionario en elementos de una lsita
+
+claves = list(en2gal.keys())
+print(claves)
 """
+

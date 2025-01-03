8ea85e5b8651c362ec348e91a12f0bb40dfc476c
Marcos <marcoschouza@gmail.com>
2025-01-03 18:09:54 +0100

    Pequeños cambios

---
+++ Programacion/1º Trimestre/Rehacer_Examenes/2ª vez/Exer5.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" 
+Débese realizar unha apliación para xestionar vivendas que ten a disposición unha inmobiliaria. Para cada vivenda débese saber a dirección na que se encontra, se esta está en venta ou en alugamento e o prezo de venta ou alugamento en cada caso.
+
+A aplicación constará do seguinte menú:
+
+    a) Ingresar datos vivenda
+    
+    
+    b) Eliminar datos vivenda
+    
+        Débese mostrar unha lista de vivendas nun menú e seleccionar a vivenda a eliminar segundo o índice co seguinte formato: <índice>) <dirección>. <venta|alugamento> - <prezo> €.
+    
+    
+    c) Modificar estado de venta a alugamento, ou viceversa
+
+        Débese mostrar unha lista de vivendas nun menú e seleccionar a vivenda a eliminar segundo o índice co seguinte formato: <índice>) <dirección>. <venta|alugamento> - <prezo> €.
+        
+    
+    d) Mostrar vivendas en alugamento
+
+        Débese mostrar co seguinte formato: <índice>) <dirección>. <venta|alugamento> - <prezo> €.
+
+
+    e) Ver o prezo de alugamento máis repetido.
+
+        En caso de empate de prezos, indica o máis baixo.
+        Pista: utiliza dicionarios para realizar dito cálculo.
+        Se non hai vivendas de alugamento devolve None
+
+
+    f) Ver o prezo de venta máis repetido.
+
+        En caso de empate de prezos, indica o máis baixo.
+        Pista: reutiliza funcións.
+        Se non hai vivendas en venda devolve None
+
+    g) Sair.
+"""
+
+__author__ = "Marcos Chouza Cruces"
+

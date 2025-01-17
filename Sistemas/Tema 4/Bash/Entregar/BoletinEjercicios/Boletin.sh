07697c7405b77c16bb6b0747c4965debe41c4676
Marcos Chouza Cruces <a23marcoscc@iessanclemente.net>
2025-01-17 09:03:44 +0100

    Sistemas

---
+++ Sistemas/Tema 4/Bash/Entregar/BoletinEjercicios/Boletin.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+mostrar_calendario(){
+
+}
+
+mostrar_palabras_lineas(){
+
+}
+
+mover_arquivo(){
+
+}
+
+renombrar_ficheiro(){
+
+}
+
+
+while (( true ))
+do
+    read -p "Ingrese a opción a escoller: " option
+
+    case $option in 
+
+        1)
+
+            ;;
+
+        2)
+
+
+            ;;
+
+        3)
+            ;;
+
+        4)
+
+            read -p "Ingrese "
+            ;;
+
+        5)
+            echo "Saíndo..."
+            break
+            ;;
+
+    esac
+
+done

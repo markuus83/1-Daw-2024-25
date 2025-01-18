c85d5ad4cf409967ceeeda7feb1340e037ab8b3e
Marcos <marcoschouza@gmail.com>
2025-01-18 23:18:51 +0100

    Programacion

--- Programacion/2º Trimestre/Tema 3/Vectores/Entregar/Exer4.java
+++ Programacion/2º Trimestre/Tema 3/Vectores/Entregar/Exer4.java
@@ -22,12 +22,68 @@ public class Exer4 {
         System.out.print("Ingrese a cantidade de alumnos: ");
         int alumno = scanner.nextInt();
 
-        System.out.print("\nIngrese a cantidade de módulos: ");
+        System.out.print("Ingrese a cantidade de módulos: ");
         int modulo = scanner.nextInt();
 
-        int[][] matriz = new int[modulo][alumno];
+        float[][] matriz = new float[alumno][modulo];
+
+        //Ingresar elemento fila por fila
+        for(int i=0; i<alumno; i++){
+            for(int j=0; j<modulo; j++){
+                System.out.print("Elemento [" + i + "][" + j +"]: ");
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+        //Amosar o contido do array bidimensional
+        for(int i=0;i<alumno; i++) {
+            for (int j = 0; j < modulo; j++) {
+                //engadimos un tabulado para aliñar as notas e mellorar a lexibilidade na salida por
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+
+        while (true) {
+
+            Scanner scanner2 = new Scanner(System.in);
+
+            System.out.println("Menu: ");
+            System.out.println("1) Media dun alumno.");
+            System.out.println("2) Porcentaxe de aprobados");
 
 
+
+            System.out.print("\nIngrese a opción a escoller desexada: ");
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    System.out.print("Ingrese o índice do alumno: ");
+                    int indice = scanner2.nextInt();
+
+                    if(indice<0 || indice>alumno){
+                        System.out.println("Erro: indice inválido.");
+                    } else{
+
+                    }
+                    
+                    
+
+                    break;
+
+                case 2:
+                    
+                    break;
+
+                default:
+                    System.out.println("Erro: ingrese unha opción válida.");
+                    break;
+            }
+            scanner2.close();
+        }
         
     }
 }

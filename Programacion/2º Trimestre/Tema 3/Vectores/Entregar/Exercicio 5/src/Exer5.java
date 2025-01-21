f3a82d87dc3835ec1ff9459e050f6e939399f7e8
Marcos Chouza Cruces <a23marcoscc@iessanclemente.net>
2025-01-21 09:07:35 +0100

    Acabado programacion

--- Programacion/2º Trimestre/Tema 3/Vectores/Entregar/Exercicio 5/src/Exer5.java
+++ Programacion/2º Trimestre/Tema 3/Vectores/Entregar/Exercicio 5/src/Exer5.java
@@ -1,4 +1,3 @@
-
 /**
  * Implante un xogo de Tres en Raia en Java utilizando Arrays bidimensionales para representar o taboleiro de xogo. O programa debe permitir que dous xogadores humanos xoguen entre eles alternando quendas ata que un deles consiga gañar ou que o taboleiro quede completo, resultando nun empate.
  * 
@@ -11,7 +10,7 @@
  *  · Amosa o taboleiro despois de cada movemento. Os espazos baleiros represéntaos con - e os xogadores con "X" para o xogador 1 e "0" para o xogador 2.
  * 
  *  · Verifica e amosa unha mensaxe cando un xogador gaña ou se hai un empate se se encheron todos os ocos.
- */
+*/
 
 import java.util.Scanner;
 
@@ -26,9 +25,11 @@ public class Exer5 {
 
         Scanner scanner = new Scanner(System.in);
 
+        
         while (true) {
-            System.out.println("\nC1\tC2\tC3");
 
+            //Amosamos o taboleiro
+            System.out.println("\nC1\tC2\tC3");
             for (int fila = 0; fila < 3; fila++) {
 
                 System.out.print("F" + fila + "\t");
@@ -48,6 +49,7 @@ public class Exer5 {
                 System.out.println();
             }
 
+            //Cando termina o xogo, indicamos quen é o gañador ou se houbo empate
             if (ganador || tableroCompleto) {
 
                 if (ganador) {
@@ -60,6 +62,7 @@ public class Exer5 {
                 break;
             }
 
+            //Pedimos por teclado as coordenadas a encher
             System.out.print("\nTurno do xogador " + jugador + ": ");
 
             int fila = scanner.nextInt();
@@ -70,31 +73,35 @@ public class Exer5 {
                 System.out.println("Erro: Movemento inválido.");
                 continue;
             }
-
             tablero[fila][columna] = jugador;
 
-            // Comprobar si hay un ganador
+            //Comprobamos se hai gañador 
             for (int i = 0; i < 3; i++) {
 
+                //Iteramos FILA por FILA para ver se hay algunha completa co mesmo signo
                 if (tablero[i][0] == jugador && tablero[i][1] == jugador
                         && tablero[i][2] == jugador) {
                     ganador = true;
                 }
 
+                //Iteramos COLUMNA por COLUMNA para ver se hay algunha completa co mesmo signo
                 if (tablero[0][i] == jugador && tablero[1][i] == jugador
                         && tablero[2][i] == jugador) {
                     ganador = true;
                 }
             }
 
+            //Comprobamos se hay unha diagonal gañadora
             if (tablero[0][0] == jugador && tablero[1][1] == jugador && tablero[2][2] == jugador) {
                 ganador = true;
             }
 
+            //Comprobamos a outra diagonal
             if (tablero[0][2] == jugador && tablero[1][1] == jugador && tablero[2][0] == jugador) {
                 ganador = true;
             }
 
+            //Comprobamos se o taboleiro está completo
             tableroCompleto = true;
             for (int i = 0; i < 3; i++) {
                 for (int j = 0; j < 3; j++) {
@@ -114,6 +121,7 @@ public class Exer5 {
                 }
             }
         }
+        //Pechamos o Scanner
         scanner.close();
     }
 }

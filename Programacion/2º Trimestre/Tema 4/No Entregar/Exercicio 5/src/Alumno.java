bad3f5ce854b65caa627b1b7a435509d80324668
Marcos <marcoschouza@gmail.com>
2025-02-03 19:32:44 +0100

    Sistemas y programacion

--- Programacion/2º Trimestre/Tema 4/No Entregar/Exercicio 5/src/Alumno.java
+++ Programacion/2º Trimestre/Tema 4/No Entregar/Exercicio 5/src/Alumno.java
@@ -3,12 +3,28 @@ public class Alumno {
     private String nome;
     private String apelido1;
     private String apelido2;
-    private byte notatrimestre1;
-    private byte notatrimestre2;
-    private byte notatrimestre3;
+    private int notatrimestre1;
+    private int notatrimestre2;
+    private int notatrimestre3;
     private int numExpediente;
 
+    //atributo para controlar el expediente
+    private static int ultimoExpediente = 100;
+
     //Constructor
+    public Alumno(String nome, String apelido1, String apelido2) {
+        this.nome = nome;
+        this.apelido1 = apelido1;
+        this.apelido2 = apelido2;
+        this.numExpediente = ++ultimoExpediente;  
+    }
+
+    public Alumno(String nome, String apelido1) {
+        this.nome = nome;
+        this.apelido1 = apelido1;
+        this.apelido2 = "";
+        this.numExpediente = ++ultimoExpediente;  
+    }
 
 
     //Setters
@@ -21,17 +37,17 @@ public class Alumno {
     public void setApelido2(String apelido2) {
         this.apelido2 = apelido2;
     }
-    public void setNotatrimestre1(byte notatrimestre1) {
+    public void setNotatrimestre1(int notatrimestre1) {
         if (notatrimestre1 >= 0 || notatrimestre1 <= 10) {
             this.notatrimestre1 = notatrimestre1;
         }
     }
-    public void setNotatrimestre2(byte notatrimestre2) {
+    public void setNotatrimestre2(int notatrimestre2) {
         if (notatrimestre2 >= 0 || notatrimestre2 <= 10) {
             this.notatrimestre2 = notatrimestre2;
         }
     }
-    public void setNotatrimestre3(byte notatrimestre3) {
+    public void setNotatrimestre3(int notatrimestre3) {
         if (notatrimestre3 >= 0 || notatrimestre3 <= 10) {
             this.notatrimestre3 = notatrimestre3;
         }
@@ -51,9 +67,9 @@ public class Alumno {
         return apelido1 + apelido2;
     }
     public double getNotamedia(){
-        return (notatrimestre1 + notatrimestre2 + notatrimestre3)/3;
+        return (notatrimestre1 + notatrimestre2 + notatrimestre3)/3.0;
     }
-    public String getAprobado(byte notatrimestre1,byte notatrimestre2, byte notatrimestre3 ){
+    public String getAprobado(){
         if (getNotamedia() >= 5) {
             return "Aprobado";
         } else {
--- Programacion/2º Trimestre/Tema 4/No Entregar/Exercicio 5/src/Main.java
+++ Programacion/2º Trimestre/Tema 4/No Entregar/Exercicio 5/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Alumno alumno1 = new Alumno("Manuel", "Varela");
+        Alumno alumno2 = new Alumno("John", "Doe");
+
+        alumno1.setNotatrimestre1(7);
+        alumno1.setNotatrimestre2(7);
+        alumno1.setNotatrimestre3(4);
+
+        System.out.println("\n" + alumno1.getApelidos() + ", " + alumno1.getNome() + " con expe. " + alumno1.getNumExpediente() + " con nota " + alumno1.getNotamedia());
+
+        System.out.println("\nO alumno " + alumno1.getNome() + " está: " + alumno1.getAprobado());
+
+        alumno1.setNotatrimestre3(9);
+
+        System.out.println("\nO alumno " + alumno1.getNome() + " está: " + alumno1.getAprobado());
+
+        System.out.println("\n" + alumno2.getApelidos() + ", " + alumno2.getNome() + " con expe. " + alumno2.getNumExpediente() + " con nota " + alumno2.getNotamedia());
     }
 }

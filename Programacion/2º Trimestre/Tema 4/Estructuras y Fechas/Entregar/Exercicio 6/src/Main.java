08820d4722bf9319822eb227cb1218291669546c
Marcos <a23marcoscc@iessanclemente.net>
2025-02-15 12:30:27 +0100

    Programacion

--- Programacion/2º Trimestre/Tema 4/Estructuras y Fechas/Entregar/Exercicio 6/src/Main.java
+++ Programacion/2º Trimestre/Tema 4/Estructuras y Fechas/Entregar/Exercicio 6/src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+/**
+ * Crea un programa en Java para xestionar as horas traballadas polos empregados. Deberás crear un HashMap onde a clave é o DNI dos empregados e o valor é unha ArrayListr para almacenar as fixaches (tanto de entrada como de saída) de dito empregado. O empregado contará co seguinte menú:
+ * 
+ *  a) Fichar entrada/saída: o usuario introducirá o seu DNI almacenarase a hora actual. Tanto as horas de entrada como de saída almacénanse nunha mesma lista, polo tanto o primeiro elemento da lista sería a fixache de entrada e o segundo a de saída. Do memo modo, o terceiro elemento sería unha fixache de entrada e o cuarto unha fixache de saída, e así sucesivamente. O resto de fixaches seguen o mesmo mecanismo.
+ * 
+ *  b) Comprobar salario: o usuario introducirá o seu DNI e indicarase o seu salario a percibir. Cada hora traballada págase a 12€, aínda que este salario se calcula en función dos minutos traballados. Para realizar o cálculo debes ter en conta tan só as xornadas terminadas, se por exemplo hai unha fixache de entrada pero non hai unha fixache de saída, non se contabilizará esa xornada.
+ */
+
+public class Main{
+    public static void main(String[] args) {
+        
     }
 }

0bba7ea85dfa54b8998efaf390f59552ebb92b62
Marcos <marcoschouza@gmail.com>
2025-03-02 19:24:00 +0100

    Sistema

---
+++ Sistemas/Tema 4/Repaso/Bash/Exercicio/Menu.sh
@@ -0,0 +1,22 @@
+#Realiza un programa en BASH que realice 5 acciones diferentes:
+
+    #1. Comprobar que la variable introducida por teclado es un directorio.
+
+    #2. Multiplicar dos números introducidos por teclado.
+
+    #3. Mostrar los números primos comprendidos entre dos números introducidos por teclado.
+
+    #4. Comparar dos cadenas introducidas por teclado.
+
+    #5. Listar el contenido del directorio home.
+    #Salir.
+
+#El programa debe de cumplir con lo siguiente:
+
+    # 3 Limpiar la pantalla antes de hacer nada.
+
+    # · Mostrar el menú con las diferentes opciones.
+    
+    # · Debe ejecutarse todo el rato hasta que el usuario decida salir del programa.
+
+    # · Tienes que subir un archivo con el código y con el siguiente formato de nombre:

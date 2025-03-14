3f24b8c5549f9bd17251d38e5b7ade574ed7746c
Marcos <a23marcoscc@iessanclemente.net>
2025-03-14 12:52:24 +0100

    Sistemas

--- Sistemas/2º Trimestre/RepasoExamen/Scripting/Powershell/Primos.ps1
+++ Sistemas/2º Trimestre/RepasoExamen/Scripting/Powershell/Primos.ps1
@@ -1,29 +1,16 @@
 function es_primo(){
-
 	if ($numero -lt 2){
-
 		Write-Host "Non é primo (PRIMEIRA PARTE)"
-
 	} else {
-
 		for($i=2; $i -le [Math]::Sqrt($numero);$i++){
-
 			if ($numero%i -ne 0){
-
 				Write-Host "É primo"
-
 			} else {
-
 				Write-Host "Non é primo (SEGUNDA PARTE)"
-
 			}
-
 		}
-
 	}
-
 }
 
 $numero = Read-Host "Ingrese un número para calacular se é primo ou non: "
-
 es_primo($numero)

1291b6a4ca853d4dc60bccb76b2fe6825524c934
Marcos <marcoschouza@gmail.com>
2025-04-03 19:27:09 +0200

    Excepciones Programacion

--- Programacion/3º Trimestre/Excepcions/probas/proba2.java
+++
@@ -1,9 +0,0 @@
-public class proba2 extends Throwable{
-    public static void main(String[] args) {
-        try {
-            int resultado = 10/0;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}

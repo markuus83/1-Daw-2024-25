bd5b88ab29ac5d601d531744cccf42f10fd2132d
Marcos <a23marcoscc@iessanclemente.net>
2025-04-05 21:20:27 +0200

    Programacion

---
+++ Programacion/3º Trimestre/ExpresionsRegulares/Exercicio2/src/EmailInvalidoException.java
@@ -0,0 +1,11 @@
+public class EmailInvalidoException extends Exception{
+    
+    /**
+     * Método constructor da clase EmailInvalidoException
+     * 
+     * @param mensaxe -> Mensaxe a mostrar
+     */
+    public EmailInvalidoException(String mensaxe){
+        super(mensaxe);
+    }
+}

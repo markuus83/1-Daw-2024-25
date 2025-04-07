7dde6894bc1275d505f74ae7c14ca467d618f815
Marcos Chouza Cruces <a23marcoscc@iessanclemente.net>
2025-04-07 10:56:20 +0200

    Programacion

--- Programacion/3º Trimestre/ExpresionsRegulares/Exercicio2/src/App.java
+++ Programacion/3º Trimestre/ExpresionsRegulares/Exercicio2/src/App.java
@@ -3,42 +3,55 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception{
 
-        ArrayList<Email> emailsNormais = new ArrayList<>();
-        ArrayList<EmailSanClemente> emailsSanClemente = new ArrayList<>();
+        ArrayList<String> emailsNormais = new ArrayList<>();
+        ArrayList<String> emailsSanClemente = new ArrayList<>();
 
-        try {
-            emailsNormais.add(new Email("maria.gomez@iessanclemente.net"));
-            emailsNormais.add(new Email("juan.perez@gmail.com"));
-            emailsNormais.add(new Email("laura.fernandez@iessanclemente.net"));
-            emailsNormais.add(new Email("laura.fernandeziessanclemente.net"));
-            emailsNormais.add(new Email("carlos.rodriguez@yahoo.com"));
-            emailsNormais.add(new Email("ana.martinez@iessanclemente.net"));
-            emailsNormais.add(new Email("pedro.lopez@hotmail.com"));
-            emailsNormais.add(new Email("sofia.rivas@iessanclemente.net"));
-            emailsNormais.add(new Email("miguel.vazquez@outlook.com"));
-            emailsNormais.add(new Email("paula.castro@iessanclemente.net"));
-            emailsNormais.add(new Email("david.sanchez@protonmail.com"));
-            emailsNormais.add(new Email("david.sanchez@protonmail"));
-        } catch (EmailInvalidoException e) {
-            System.out.println(e.getMessage());
-        }
+        //Inicializamos un ArrayList con String para posteriormente convertilo en obxectos
+        emailsNormais.add("maria.gomez@iessanclemente.net");
+        emailsNormais.add("juan.perez@gmail.com");
+        emailsNormais.add("laura.fernandez@iessanclemente.net");
+        emailsNormais.add("laura.fernandeziessanclemente.net");
+        emailsNormais.add("carlos.rodriguez@yahoo.com");
+        emailsNormais.add("ana.martinez@iessanclemente.net");
+        emailsNormais.add("pedro.lopez@hotmail.com");
+        emailsNormais.add("sofia.rivas@iessanclemente.net");
+        emailsNormais.add("miguel.vazquez@outlook.com");
+        emailsNormais.add("paula.castro@iessanclemente.net");
+        emailsNormais.add("david.sanchez@protonmail.com");
+        emailsNormais.add("david.sanchez@protonmail");
+       
+        //Inicializamos un ArrayList con String para posteriormente convertilo en obxectos
+        emailsSanClemente.add("maria.gomez@iessanclemente.net");
+        emailsSanClemente.add("juan.perez@gmail.com");
+        emailsSanClemente.add("laura.fernandez@iessanclemente.net");
+        emailsSanClemente.add("laura.fernandeziessanclemente.net");
+        emailsSanClemente.add("carlos.rodriguez@yahoo.com");
+        emailsSanClemente.add("ana.martinez@iessanclemente.net");
+        emailsSanClemente.add("pedro.lopez@hotmail.com");
+        emailsSanClemente.add("sofia.rivas@iessanclemente.net");
+        emailsSanClemente.add("miguel.vazquez@outlook.com");
+        emailsSanClemente.add("paula.castro@iessanclemente.net");
+        emailsSanClemente.add("david.sanchez@protonmail.com");
+        emailsSanClemente.add("david.sanchez@protonmail");
         
-        try {
-            emailsSanClemente.add(new EmailSanClemente("maria.gomez@iessanclemente.net"));
-            emailsSanClemente.add(new EmailSanClemente("juan.perez@gmail.com"));
-            emailsSanClemente.add(new EmailSanClemente("laura.fernandez@iessanclemente.net"));
-            emailsSanClemente.add(new EmailSanClemente("laura.fernandeziessanclemente.net"));
-            emailsSanClemente.add(new EmailSanClemente("carlos.rodriguez@yahoo.com"));
-            emailsSanClemente.add(new EmailSanClemente("ana.martinez@iessanclemente.net"));
-            emailsSanClemente.add(new EmailSanClemente("pedro.lopez@hotmail.com"));
-            emailsSanClemente.add(new EmailSanClemente("sofia.rivas@iessanclemente.net"));
-            emailsSanClemente.add(new EmailSanClemente("miguel.vazquez@outlook.com"));
-            emailsSanClemente.add(new EmailSanClemente("paula.castro@iessanclemente.net"));
-            emailsSanClemente.add(new EmailSanClemente("david.sanchez@protonmail.com"));
-            emailsSanClemente.add(new EmailSanClemente("david.sanchez@protonmail"));
-        } catch (EmailInvalidoException e) {
-            System.out.println(e.getMessage());
+        //Capturamos erros
+        System.out.println("\nEmails normais: ");
+        for (String string : emailsSanClemente) {
+            try {
+                new Email(string);
+            } catch (EmailInvalidoException e) {
+                System.out.println("\t"+e.getMessage());
+            }
         }
-        
+
+        //Capturamos erros
+        System.out.println("\nEmail dominio San Clemente: ");
+        for (String string : emailsSanClemente) {
+            try {
+                new EmailSanClemente(string);
+            } catch (EmailInvalidoException e) {
+                System.out.println("\t"+e.getMessage());
+            }
+        }
     }
 }

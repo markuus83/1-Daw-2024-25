a2ede3d7b2d47ced58e27cf431e2c870a7993a25
Marcos Chouza Cruces <a23marcoscc@iessanclemente.net>
2025-04-25 12:54:40 +0200

    progra

--- Programacion/3º Trimestre/Final/TendaMusica/src/App.java
+++ Programacion/3º Trimestre/Final/TendaMusica/src/App.java
@@ -1,7 +1,7 @@
 import Vista.MenuInicio;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         new MenuInicio().run();
     }
 }

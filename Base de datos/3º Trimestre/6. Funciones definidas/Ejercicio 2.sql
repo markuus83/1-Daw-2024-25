80ac60bdbdc469d31bd4468fde77fce62b57ddb3
daw1 - Chouza Cruces <A23MarcosCC@sanclemente.local>
2025-05-15 11:47:23 +0200

    BBDD

--- Base de datos/3º Trimestre/6. Funciones definidas/Ejercicio 2.sql
+++ Base de datos/3º Trimestre/6. Funciones definidas/Ejercicio 2.sql
@@ -4,21 +4,89 @@ use a23marcoscc_BD_Almacen_Procedimientos;
 --(que funcione como un autoincremento  para el IdDepartamento), es decir, cuando se llama a la función, 
 --devolverá el máximo número de departamento existente en la tabla más 1.
 
+	-- Observamos os diferentes ids
+	SELECT IDDepartamento FROM DEPARTAMENTOS
 
+	-- Comezamos a función
+	CREATE OR ALTER FUNCTION dbo.siguienteNumDepartamento()
+	RETURNS INT
+
+		BEGIN
+
+			DECLARE @v_maximo INT;
+
+				SELECT @v_maximo = MAX(IDDepartamento )
+				FROM DEPARTAMENTOS
+			RETURN @v_maximo+10
+		END;
+
+	GO
+
+	PRINT 'El siguiente id que se tendrá que usar será: '+CONVERT(VARCHAR(10),dbo.siguienteNumDepartamento())
 
 
 --2. Crea una función que devuelva el nombre del jefe de un empleado. La función aceptará como parámetro 
 --un número de empleado y buscará el nombre del jefe de ese empleado y devolverá ese valor.
 
 
+	SELECT * FROM EMPLEADOS;
 
+	CREATE OR ALTER FUNCTION dbo.nombreXefe(@id AS INT)
+	RETURNS VARCHAR(30) AS
+		BEGIN
+
+			DECLARE @nomeXefe AS VARCHAR(30);
+
+			SELECT @nomeXefe = x.Nombre
+			FROM EMPLEADOS e JOIN EMPLEADOS x
+			ON e.DIRECTOR = x.IDEmpleado
+			WHERE @id = e.IDEmpleado
+
+			return @nomeXefe
+		END
+	GO
+
+	PRINT 'O nome do director do empregado con id -> "7521" é: '+convert(varchar(30),dbo.nombreXefe(7521));
 
 
 -- 3. Modifica la función anterior para que en caso de que no encuentre un jefe válido devuelva el valor 
 --“Jefe no encontrado”.
 
 
+	CREATE OR ALTER FUNCTION dbo.nombreXefeModificado(@id AS INT)
+	RETURNS VARCHAR(30) AS
+		BEGIN
 
+			DECLARE @nomeXefe AS VARCHAR(30);
+			DECLARE @resposta AS VARCHAR(30);
+
+				IF EXISTS (
+					SELECT x.Nombre
+					FROM EMPLEADOS e JOIN EMPLEADOS x
+					ON e.DIRECTOR = x.IDEmpleado
+					WHERE @id = e.IDEmpleado)
+
+					BEGIN
+						SELECT @nomeXefe = x.Nombre
+						FROM EMPLEADOS e JOIN EMPLEADOS x
+						ON e.DIRECTOR = x.IDEmpleado
+						WHERE @id = e.IDEmpleado
+					END
+					
+				ELSE
+					BEGIN
+						set @nomeXefe = 'Jefe no encontrado'
+					END
+
+			return @nomeXefe
+		END
+	GO
+
+	SELECT * FROM EMPLEADOS;
+	
+	PRINT 'O nome do director do empregado con id->"7521" é: '+convert(varchar(30),dbo.nombreXefeModificado(7521))
+	PRINT 'O nome do director do empregado con id->"1" é: '+convert(varchar(30),dbo.nombreXefeModificado(1))
+	PRINT 'O nome do director do empregado con id->"7839" é: '+convert(varchar(30),dbo.nombreXefeModificado(7839))
 
 
 -- 4. Desarrollar un procedimiento que visualice el apellido, 
@@ -26,14 +94,19 @@ use a23marcoscc_BD_Almacen_Procedimientos;
 --Para mostrar el nombre del jefe deberá utilizar la función creada en el ejercicio 3.
 
 
+	CREATE OR ALTER PROCEDURE pr_verJefe
+	AS
+		SELECT Nombre,dbo.nombreXefeModificado(idempleado) as nombreXefe, CAST(fechaalta as date) as fecha
+		FROM EMPLEADOS
+		
 
-
-
+	EXEC pr_verJefe
 --5. Escribir un programa que visualice el apellido y el salario de los cinco empleados 
 --que tienen el salario más alto.
 
 
-
+	CREATE OR ALTER FUNCTION dbo.mostrarDatos()
+	RETURNS 
 
 
 --6. Crea un procedimiento que acepte un número de departamento y devuelva
@@ -70,3 +143,15 @@ use a23marcoscc_BD_Almacen_Procedimientos;
 	--b. nombre: el nombre del departamento tendrá el formato departamento X, 
 				--siendo X el valor asignado como número de departamento.
 	--c. Localidad: se asignará el valor pasado como parámetro.
+
+
+
+
+
+
+
+
+
+
+
+
